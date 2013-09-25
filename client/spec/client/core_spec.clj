e5678bc665de7287a54fa854d09d2a3c3efb0784
Waldemar <waldemar.schwan@adcloud.com>
2013-09-25 22:09:16 +0200

    implemented meta data parsing

---
+++ client/spec/client/core_spec.clj
@@ -0,0 +1,29 @@
+(ns client.core-spec
+  (:require [client.core :refer :all]
+            [speclj.core :refer :all]))
+
+(describe "filepath->metadata"
+  (with castle-mkv "Castle (2009)/Castle (2009) - 5x01 - After The Storm.mkv")
+  (with castle-banner "Castle (2009)/banner.jpg")
+  (with true-bood-nfo "/True Blood/Season 06/True Blood - S06E05 - Let's Boot and Rally - HD TV.nfo")
+
+  (it "finds the season number"
+    (should= 5 (:season# (filepath->metadata @castle-mkv)))
+    (should= 6 (:season# (filepath->metadata @true-bood-nfo))))
+  (it "finds the episode number"
+    (should= 1 (:episode# (filepath->metadata @castle-mkv)))
+    (should= 5 (:episode# (filepath->metadata @true-bood-nfo))))
+  (it "finds the showname"
+    (should= "Castle (2009)" (:showname (filepath->metadata @castle-mkv)))
+    (should= "True Blood"    (:showname (filepath->metadata @true-bood-nfo))))
+  (it "finds the title"
+    (should= "After The Storm"      (:title (filepath->metadata @castle-mkv)))
+    (should= "Let's Boot and Rally" (:title (filepath->metadata @true-bood-nfo))))
+  (it "contains the full filename"
+    (should= @castle-mkv    (:filepath (filepath->metadata @castle-mkv)))
+    (should= @true-bood-nfo (:filepath (filepath->metadata @true-bood-nfo))))
+  (it "containd the file extention"
+    (should= "mkv" (:ext (filepath->metadata @castle-mkv)))
+    (should= "nfo" (:ext (filepath->metadata @true-bood-nfo))))
+  (it "returns nil in case data can not be parsed"
+    (should (nil? (filepath->metadata @castle-banner)))))
--- client/src/client/core.clj
+++ client/src/client/core.clj
@@ -1,6 +1,21 @@
 (ns client.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn filepath->metadata [filepath]
+  {:pre [[string? filepath]]}
+  (let [pattern (re-pattern
+                  (str
+                    "(.*/)?"                     ; path / prefix
+                    "([\\w\\(\\) ]*)"            ; show name
+                    " - [Ss]?(\\d*)[Eex]?(\\d*)" ; season# episode#
+                    "( - ([^-]*))?"              ; episode title
+                    "( - ([^-]*))?"              ; quality
+                    "(.*)"                       ; other suff
+                    "\\.(\\w+)"))                ; file extension
+        groups (re-matches pattern filepath)]
+    (if (not (nil? groups))
+      {:filepath filepath
+       :showname (groups 2)
+       :title    (clojure.string/trim (groups 6))
+       :season#  (Integer/parseInt (groups 3))
+       :episode# (Integer/parseInt (groups 4))
+       :ext      (groups 10)})))
