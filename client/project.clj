c2c73ecf56af64f0f5abeb61963092e16d420b44
Waldemar <waldemar.schwan@adcloud.com>
2013-09-25 22:08:53 +0200

    added testing dependenvies

--- client/project.clj
+++ client/project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [speclj "2.7.4"]]
+  :plugins [[speclj "2.5.0"]]
+  :test-paths ["spec"])
