467c05fd1a4af33a256f0a6888b3d11825314987
Waldemar <waldemar.schwan@adcloud.com>
2013-09-25 22:18:56 +0200

    added file systme lib + changed main namespace

--- client/project.clj
+++ client/project.clj
@@ -4,6 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [me.raynes/fs "1.4.4"]
                  [speclj "2.7.4"]]
   :plugins [[speclj "2.5.0"]]
+  :main client.core
   :test-paths ["spec"])
