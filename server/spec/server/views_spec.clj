baec4c60bbfc9a7a5e619484a2c06279cf5b337a
Waldemar <waldemar.schwan@adcloud.com>
2013-10-05 22:00:56 +0200

    correct encoding for shar links

--- server/spec/server/views_spec.clj
+++ server/spec/server/views_spec.clj
@@ -2,14 +2,14 @@
   (:require [speclj.core :refer :all]
             [server.views :refer :all]))
 
-(describe "render-show"
+(describe "show->str"
   (with show {"showname" "True Blood"
               "title"    "Kill em all!"
               "season#"  5
               "episode#" 2})
-  (it "converts a show map into a string containing showname, season and episode"
-    (should= "True Blood - S05E02"
-             (render-show @show))))
+  (it "converts a show map into a string"
+    (should= "True Blood S05E02 - Kill em all!"
+             (show->str @show))))
 
 (describe "render-shows"
   (with shows {"timmy" [{"showname" "True Blood"
@@ -25,8 +25,17 @@
                          "season#"  5
                          "episode#" 2}]})
   (it "takes a map indext by client as well as a show renderer and turns a hiccup unordered list"
-    (should= ["True Blood - S05E02" "True Blood - S05E03"]
-             (render-shows @shows render-show)))
+    (should= 2
+             (count (render-shows @shows render-show))))
   (it "is robust agains an empty map"
     (should= []
              (render-shows {} render-show))))
+
+(describe "share-show-url"
+  (with show {"showname" "True Blood"
+                         "title"    "Kill em all!"
+                         "season#"  5
+                         "episode#" 2})
+  (it "returns the url as a relative path"
+    (should= "/share/True%20Blood/5/2"
+             (share-show-url @show))))
--- server/src/server/views.clj
+++ server/src/server/views.clj
@@ -1,17 +1,36 @@
 (ns server.views
   (:require [clojure.string :as string]
+            [hiccup.element :refer [link-to]]
+            [hiccup.util :refer [url url-encode]]
             [clojure.pprint :refer [pprint]]))
+
+(defn share-show-url
+  [show]
+  (format "/share/%s/%d/%d"
+          (string/replace (show "showname")
+                          #" "
+                          "%20")
+          (show "season#")
+          (show "episode#")))
+
+(defn show->str [show]
+  (format "%s S%02dE%02d - %s"
+          (show "showname")
+          (show "season#")
+          (show "episode#")
+          (show "title")))
 
 (defn render-show
   "Converts a map containing show information into a string."
   [show]
-  (format "%s - S%02dE%02d"
-          (show "showname")
-          (show "season#")
-          (show "episode#")))
+  (let [showname (show "showname")
+        season   (show "season#")
+        episode  (show "episode#")
+        link-name (show->str show)]
+    (link-to (share-show-url show)
+             link-name)))
 
 (defn render-shows [shows show-renderer]
-  (pprint shows)
   (->> (vals shows)
        flatten
        (map show-renderer)
